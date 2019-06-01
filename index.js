130add408452b17f6a2483a111fcbb6fc9b9eb5c
Louis <woodylouis@users.noreply.github.com>
2019-06-01 19:30:57 +1000

    fixing herolu

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
 // "./models/User" and "./services/passport", order is critical. Error will appear if incorrect order
-require("./models/User");
+require("./models/user");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
