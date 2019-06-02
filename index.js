b5e7560aa09a51c964cb85a798082691be140bdf
Louis <woodylouis@users.noreply.github.com>
2019-06-03 00:00:49 +1000

    Payment in test mode Done and display credit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 // "./models/User" and "./services/passport", order is critical. Error will appear if incorrect order
 require("./models/user");
@@ -12,6 +13,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 //middleware
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // cookie stays 30 days Maximum
@@ -23,6 +25,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
