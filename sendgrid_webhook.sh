123cff10290614286f3e6205096ac24380d71434
Louis <woodylouis@users.noreply.github.com>
2019-06-05 22:37:29 +1000

    when recipients click 'yes', return more clean data from sendgrid

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,6 +1,6 @@
 # restart localtunnel if it crashes
 function localtunnel {
-  lt -s bvaefjkds --port 5000
+  lt -s vbekhdfkjdfhsjzx --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
