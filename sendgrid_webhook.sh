6d6e0985d1a2def000806ad1e83a3496b6e708dd
Louis <woodylouis@users.noreply.github.com>
2019-06-06 15:51:32 +1000

    users can see survey result

--- sendgrid_webhook.sh
+++ sendgrid_webhook.sh
@@ -1,6 +1,6 @@
 # restart localtunnel if it crashes
 function localtunnel {
-  lt -s viabdjssdnclkadslclfad --port 5000
+  lt -s fbieckcds --port 5000
 }
 until localtunnel; do
 echo "localtunnel server crashed"
